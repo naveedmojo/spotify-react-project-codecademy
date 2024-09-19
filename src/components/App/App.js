946043407e4d5988a885d316fcb34c4875580899
naveedmojo <naveedmojojo@gmail.com>
2024-09-19 19:57:42 +0530

    test commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
-import SearchBar from '../SearchBar/SearchBar';
+
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <h1 className='center'>Jammming</h1>
-      <SearchBar />
+    
       <div className={styles.resultsAndPlaylist}>
         <div className={styles.searchResultsContainer}>
           <SearchResults  setplaylist={setPlayList} />
