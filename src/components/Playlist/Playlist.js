ebea496e4fc1593d940cff29f85fd6469cf88283
naveedmojo <naveedmojojo@gmail.com>
2024-09-19 16:47:12 +0530

    completed the spotify application with 30% bugs

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,25 +1,51 @@
 import React,{useState} from 'react';
 import styles from './Playlist.module.css';
 import Tracklist from '../Tracklist/Tracklist';
-import LoginSpotify from "../LoginSpotify";
+
 
 function Playlist({playlist,setplaylist}) {
   const [playlistName,setPlaylistName]=useState("My Playlist")
+  function generateRandomString(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+  function loginSpotify(playList){
+      console.log(playList)
+      const client_id = '3a6a48e95f5c4d37ac533f5471a4702e';
+      let redirect_uri = 'http://localhost:3001';
+      const stateKey = 'spotify_auth_state';
+      const state = generateRandomString(16);
+
+      localStorage.setItem(stateKey, state);
+      let scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+
+      let authorizeurl = 'https://accounts.spotify.com/authorize';
+      authorizeurl += '?response_type=token';
+      authorizeurl += '&client_id=' + encodeURIComponent(client_id);
+      authorizeurl += '&scope=' + encodeURIComponent(scope);
+      authorizeurl += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+      authorizeurl += '&state=' + encodeURIComponent(state);
+      window.location.href = authorizeurl;
+    }
   function namechangehandler(event){
     setPlaylistName(event.target.value)
   }
   function saveToSpotify(e){
     e.preventDefault()
-    if(!playlistName){
-      alert("name your playlist")
+    if(!playlistName || !playlist){
+      alert("name your playlist or playlist empty")
       return
     }
-    <LoginSpotify/>
-    console.log(playlist)
+    loginSpotify(playlist)
+    
   }
   
   
-    function removeTrack(trackId) {
+  function removeTrack(trackId) {
       setplaylist((prev) => prev.filter((track) => track.id !== trackId));
     }
   
